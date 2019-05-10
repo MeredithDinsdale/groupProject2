60e3555628708c147c96485adbd9019d919117f3
Nash McDonald <nash42192@gmail.com>
2019-05-09 20:33:14 -0700

    added more rpi api routes

--- 04-Stu_Project-Starter/Unsolved/models/rpi.js
+++ 04-Stu_Project-Starter/Unsolved/models/rpi.js
@@ -9,7 +9,7 @@ module.exports = function(sequelize, DataTypes) {
       allowNull: false
     },
     mac_address: {
-      type: DataTypes.INTEGER,
+      type: DataTypes.STRING,
       allowNull: false
     }
   });
